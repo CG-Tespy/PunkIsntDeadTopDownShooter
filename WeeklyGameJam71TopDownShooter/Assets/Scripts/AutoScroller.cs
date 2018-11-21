c6b634be6975d4d62d02fd12ba7e618452e5cfc2
Baradoros <thatfunderburkguy@gmail.com>
2018-11-21 16:28:53 -0600

    Added scrolling background

---
+++ WeeklyGameJam71TopDownShooter/Assets/Scripts/AutoScroller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScroller : MonoBehaviour {
+
+    public float speed = 3;
+
+    void FixedUpdate() {
+        transform.position -= new Vector3(0, speed / 100, -0.01f);
+    }
+}
---
+++ WeeklyGameJam71TopDownShooter/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour {
+
+    public GameObject background;
+
+    private List<GameObject> bgList = new List<GameObject>();
+    private float distance;
+    private bool spawn = false;
+
+    void Start() {
+        distance = background.GetComponent<SpriteRenderer>().bounds.size.y - 0.05f;
+        GameObject bg = Instantiate(background, Vector3.zero, Quaternion.identity);
+        bgList.Add(bg);
+        Spawn();
+    }
+
+    void Update() {
+        foreach(GameObject bg in bgList) {
+            if (bg.transform.position.y < -distance * 1.5) {
+                bgList.Remove(bg);
+                Destroy(bg);
+                break;
+            }
+        }
+
+        foreach(GameObject bg in bgList) {
+            if (bg.transform.position.y > 0) {
+                spawn = false;
+                break;
+            } else {
+                spawn = true;
+            }
+        }
+
+        if (spawn) {
+            Spawn();
+        }
+    }
+
+    private void Spawn() {
+        GameObject bg = Instantiate(background, new Vector3(0, distance, 0), Quaternion.identity);
+        bgList.Add(bg);
+    }
+}
