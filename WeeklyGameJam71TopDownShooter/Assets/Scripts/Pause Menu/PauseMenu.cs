71d99cf2238bf0de42dd989fad0e09bd2f4c6bd7
Baradoros <thatfunderburkguy@gmail.com>
2018-11-21 19:41:04 -0600

    Added pause menu

---
+++ WeeklyGameJam71TopDownShooter/Assets/Scripts/Pause Menu/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject pauseMenu;
+    public AudioSource audio;
+    public AudioClip clip;
+
+    private bool pausable = true;
+
+    void Start() {
+        pauseMenu.SetActive(false);
+    }
+
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape) && !pauseMenu.activeInHierarchy && pausable) {
+            audio.PlayOneShot(clip);
+            PauseGame();
+            pausable = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && pauseMenu.activeInHierarchy && pausable) {
+            audio.PlayOneShot(clip);
+            ContinueGame();
+            pausable = false;
+        }
+        if (Input.GetKeyUp(KeyCode.Escape))
+            pausable = true;
+	}
+
+    public void PauseGame() {
+        Time.timeScale = 0.0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void ContinueGame() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void QuitToMenu() {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Title Screen", LoadSceneMode.Single);
+    }
+}
