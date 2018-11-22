857dc32c7da4c5b3cdb09b4d6db9467f9df5aad9
CG-Tespy <gabriel.cabrera257@hotmail.com>
2018-11-22 11:05:11 -0500

    Slight edits

---
+++ WeeklyGameJam71TopDownShooter/Assets/Scripts/RicochetingBullet2D.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RicochetingBullet2D : RotatingBullet2D 
+{
+	[Tooltip("How many times this bullet can ricochet before being allowed to die.")]
+	[SerializeField] int ricochetCount = 					1;
+	int ricochetsDone = 0;
+
+	protected override void OnCollisionEnter2D(Collision2D other)
+	{
+		contactEvents.CollisionEnter2D.Invoke(other);
+	}
+	
+}
